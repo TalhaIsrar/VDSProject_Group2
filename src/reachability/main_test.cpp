f2219215c96500710d7bde569856d9c01fa666b9
TalhaIsrar <talhaisrar02@gmail.com>
2025-06-26 22:45:39 +0200

    Added constructor test

--- src/reachability/main_test.cpp
+++ src/reachability/main_test.cpp
@@ -7,20 +7,34 @@
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS();  
+}
 
-    // std::unique_ptr<ClassProject::ReachabilityInterface> fsm2 = std::make_unique<ClassProject::Reachability>(2);
-    // std::vector<BDD_ID> stateVars2 = fsm2->getStates();
-    // std::vector<BDD_ID> transitionFunctions;
-    // BDD_ID s0 = stateVars2.at(0);
-    // BDD_ID s1 = stateVars2.at(1);    
-    // transitionFunctions.push_back(fsm2->neg(s0)); // s0' = not(s0)
-    // transitionFunctions.push_back(fsm2->neg(s1)); // s1' = not(s1)
-    // fsm2->setTransitionFunctions(transitionFunctions);
-    // fsm2->setInitState({false,false});
 
-    // bool b = fsm2->isReachable({false, false}); // true
-    // b = fsm2->isReachable({false, true}); // true
-    // b = fsm2->isReachable({true, false}); // false
-    // b = fsm2->isReachable({true, true}); // false   
+TEST_F(ReachabilityTest, HowTo_Example) { /* NOLINT */
+
+    BDD_ID s0 = stateVars2.at(0);
+    BDD_ID s1 = stateVars2.at(1);
+
+    transitionFunctions.push_back(fsm2->neg(s0)); // s0' = not(s0)
+    transitionFunctions.push_back(fsm2->neg(s1)); // s1' = not(s1)
+    fsm2->setTransitionFunctions(transitionFunctions);
+
+    fsm2->setInitState({false,false});
+
+    ASSERT_TRUE(fsm2->isReachable({false, false}));
+    ASSERT_FALSE(fsm2->isReachable({false, true}));
+    ASSERT_FALSE(fsm2->isReachable({true, false}));
+    ASSERT_TRUE(fsm2->isReachable({true, true}));
+}
+
+TEST_F(ReachabilityTest, Constructor_Test)
+{
+    for (int i = 0; i < stateVars2.size(); i++)
+    {
+        ASSERT_EQ(fsm2->getTopVarName(stateVars2.at(i)), "s" + std::to_string(i));
+    }
+
+    ASSERT_THROW(std::make_unique<ClassProject::Reachability>(0), std::runtime_error);
+    ASSERT_THROW(std::make_unique<ClassProject::Reachability>(0, 0), std::runtime_error);
 }
